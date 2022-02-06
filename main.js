4f8c84c65ca7b1ea4ff86b51b333ae4ea4fc7319
Andrey Petrovskiy <50797137+petrovvvic@users.noreply.github.com>
2022-02-06 15:47:17 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,55 +1,33 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js';
-
 import { FBXLoader } from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/loaders/FBXLoader.js';
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js';
 
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(95, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-cube.scale.set(2, 2, 1);
-// scene.add(cube);
-
-
-camera.position.set(0, 0, 1)
 const controls = new OrbitControls(camera, renderer.domElement);
 //controls.update() must be called after any manual changes to the camera's transform
-
-controls.update();
 controls.update();
 
-
-
-scene.background = new THREE.Color(0xf);
+scene.background = new THREE.Color(0xffff);//you can change background color here
 
 const loader = new GLTFLoader();
-
-loader.load('model.gltf', function(model) {
-    model.scene.scale.set(0.1, 0.1, 0.1);
-    // model.position.set(0, 0, 0);
-
-    console.log(model.scene);
-
-
+loader.load('$put your model name here, example : model.gltf$', function(model) {
+//     model.scene.scale.set(0.1, 0.1, 0.1);  if your model is not displaying , uncomment this line
     scene.add(model.scene);
     window.model = model;
     renderer.render(scene, camera);
-
-
-
 }, undefined, function(error) {
-
+    // if loader cant load your model, this will throw an error
     console.error(error);
-
 });
+
 const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444, 0.4);
 hemiLight.position.set(0, 20, 0);
 scene.add(hemiLight);
@@ -61,16 +39,9 @@ scene.add(dirLight);
 light.position.set(2, 2, 5);
 scene.add(light);
 
-
 function animate() {
-
     requestAnimationFrame(animate);
-
-    // required if controls.enableDamping or controls.autoRotate are set to true
     controls.update();
-
     renderer.render(scene, camera);
-
 }
-
 animate();
